9b0c2e9f73264eedf4a106e925d048cc8b23b2e8
Suley <luzhang@jmlvyou.com>
2013-10-07 09:16:35 +0800

    Small changes

--- trunk/jinmi/protected/controllers/CartController.php
+++ trunk/jinmi/protected/controllers/CartController.php
@@ -2,13 +2,34 @@
 
 class CartController extends Controller{
 	public function actionAdd(){
-
+		if(isset($_POST['CartForm'])){
+			$cart = new Cart();
+			$cart->attributes = $_POST['CartForm'];
+			$cart->user_id = Yii::app()->user->id;
+			if($cart->save()){
+				if(Yii::app()->request->isAjaxRequest){
+					echo json_encode('saved');
+				}else{
+					throw new CHttpException(500, 'Server error occurs, could not save your cart item this time');
+				}
+			}
+			
+		}
+		
 	}
-	public function actionRemove(){
-
+	public function actionRemove($id){
+		$cart = new Cart();
+		if($cart->deleteItem($id, Yii::app()->user->id)){
+			if(Yii::app()->request->isAjaxRequest){
+				echo json_encode('deleted');
+			}else{
+				throw new CHttpException(500, 'Server error occurs, could not delete your cart item this time');
+			}
+		}
 	}
 	public function actionCheckout(){
-
+		$order = new Order();
+		$order->checkout(Yii::app()->user->id);
 	}
 	public function actionChange(){
 		
--- trunk/jinmi/protected/models/User.php
+++ trunk/jinmi/protected/models/User.php
@@ -1,6 +1,18 @@
 <?php
 
 class User extends CActiveRecord{
-	
+	public $username, $password;
+	private $salt = 'qigjewoijfiocnonewo';
+	public function tableName(){
+		return 'user';
+	}
+	public static function model($className = __CLASS){
+		return parent::model($className);
+	}
+	public function hashPassword($password, $salt = 0){
+		if(isset($salt))
+			$salt = $this->salt;
+		return md5($password.$salt);
+	}
 }
 ?>
