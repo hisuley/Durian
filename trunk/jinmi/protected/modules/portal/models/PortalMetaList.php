a5777484fc1f1a345c64c20c003b53f5b04f3130
Lu Zhang <luzhang@jmlvyou.com>
2013-11-20 11:51:20 +0800

    Add meta support to store some meta value

---
+++ trunk/jinmi/protected/modules/portal/models/PortalMetaList.php
@@ -0,0 +1,29 @@
+<?php
+
+class PortalMetaList extends CActiveRecord{
+	public $id, $name, $description, $default_value, $type, $create_time;
+	public static function model($className = __CLASS__){
+		return parent::model($className);
+	}
+	public function tables(){
+		return 'portal_meta_list';
+	}
+	public function rules(){
+		return array(
+			array('name, description, default_value, type,  create_time', 'safe')
+			);
+	}
+	public function relations(){
+		return array(
+			'value'=>array(self::HAS_MANY, 'PortalMeta', 'meta_id');
+			);
+	}
+	public function beforeSave(){
+		if($this->isNewRecord){
+			$this->create_time = strtotime('now');
+		}
+		return true;
+	}
+	
+}
+?>
