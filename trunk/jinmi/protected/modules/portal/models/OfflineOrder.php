ac2a9bc72d05598f19676aad660b30843d193fdb
Lu Zhang <luzhang@jmlvyou.com>
2013-11-20 11:39:08 +0800

    Add sub-status to offline order model

--- trunk/jinmi/protected/modules/portal/models/OfflineOrder.php
+++ trunk/jinmi/protected/modules/portal/models/OfflineOrder.php
@@ -8,34 +8,52 @@
  **/
 
 class OfflineOrder extends CActiveRecord{
-    public $pay_status, $status, $amount;
+    public $pay_status, $status, $sub_status, $amount;
     public $create_time;
-    const STATUS_SUCCESS = 'order_success';
-    const STATUS_PROFILE_RECEIVED = 'profile_received';
-    const STATUS_VERIFYING = 'verifying';
-    const STATUS_REJECT = 'reject';
-    const STATUS_SEND_VISA = 'send_visa';
-    const STATUS_PROFILE_INCOMPLETE = 'profile_incomplete';
-    const STATUS_SEND_BACK = 'send_back';
-    const STATUS_VISA_RETURN = 'visa_return';
+    
+    /**
+     * Define order's type
+     **/
+    const TYPE_VISA = 'visa';
+    public static $typeIntl = array(self::TYPE_VISA => '签证');
+    
+    /**
+     * Define payment's status
+     **/
+    const PAY_OK = 1;
+    const PAY_NO = 0
+    
+    /**
+     * Define Global status of order
+     * */
+    const STATUS_NEW = 'new_order';
+    const STATUS_ONGOING = 'ongoing';
     const STATUS_COMPLETE = 'complete';
-    const STATUS_FINANCE_VERIFY = 'finance_verify';
-    const STATUS_OPERATE_VERIFY = 'operate_verify';
-    const STATUS_ACCEPT = 'accept';
-    private static $statusIntl = array(
-        self::STATUS_SUCCESS => '下单成功',
-        self::STATUS_OPERATE_VERIFY => '操作审核',
-        self::STATUS_FINANCE_VERIFY => '财务审核',
-        self::STATUS_PROFILE_INCOMPLETE => '资料不全，退回',
-        self::STATUS_PROFILE_RECEIVED => '收到资料',
-        self::STATUS_REJECT => '拒签',
-        self::STATUS_ACCEPT => '出签',
-        self::STATUS_SEND_BACK => '已寄回',
-        self::STATUS_VERIFYING => '资料审核',
-        self::STATUS_SEND_VISA => '送签',
-        self::STATUS_VISA_RETURN => '归还',
-        self::STATUS_COMPLETE => '完结'
-    );
+    public static $statusIntl = array(self::STATUS_NEW => '下单成功',self::STATUS_ONGOING => '进行中', self::STATUS_COMPLETE => '订单完成');
+    
+    /** Sub order-status defination **/
+    /**
+     * Visa Order Sub-Status
+     * 
+     * 流程：销售下单(new_order)->
+     *      操作确认(operate_confirm)->
+     *      送签人确认(sender_confirm)->
+     *      出签(issue_visa)->
+     *      送签人确认(sender_issue_confirm)->
+     *      送出(visa_sent)->
+     *      确认收到(visa_received)->
+     *      订单完成(complete)(需满足订单支付条件)
+     * Define Visa Order Sub-Status **/
+
+    const SUBSTATUS_VISA_NEW = 'new_order';
+    const SUBSTATUS_VISA_OPERATE_CONFIRM = 'operate_confirm';
+    const SUBSTATUS_VISA_SENDER_CONFIRM = 'sender_confirm';
+    const SUBSTATUS_VISA_ISSUE_VISA = 'issue_visa';
+    const SUBSTATUS_VISA_SENDER_ISSUE_CONFIRM = 'sender_issue_confirm';
+    const SUBSTATUS_VISA_VISA_SENT = 'visa_sent';
+    const SUBSTATUS_VISA_VISA_RECEIVED = 'visa_received';
+    const SUBSTATUS_VISA_COMPLETE = 'complete';
+    public static $subStatus_visa = array(self::SUBSTATUS_VISA_NEW => '销售下单', self::SUBSTATUS_VISA_OPERATE_CONFIRM => '操作确认', self::SUBSTATUS_VISA_SENDER_CONFIRM => '送签人确认', self::SUBSTATUS_VISA_ISSUE_VISA => '出签', self::SUBSTATUS_VISA_SENDER_ISSUE_CONFIRM => '送签人确认', self::SUBSTATUS_VISA_VISA_SENT => '签证送出', self::SUBSTATUS_VISA_VISA_RECEIVED => '签证收到', self::SUBSTATUS_VISA_COMPLETE => '完成订单');
 
     public static function model($className = __CLASS__){
         return parent::model($className);
@@ -51,14 +69,23 @@ class OfflineOrder extends CActiveRecord{
     }
     public function rules(){
         return array(
-          array('name, type, amount, total_price, pay_status, status, user_id, create_time', 'safe')
+          array('name, type, amount, total_price, pay_status, status, sub_status, user_id, create_time', 'safe')
         );
     }
+    /**
+     * Some useful action before saving the data 
+     * 
+     **/
     public function beforeSave(){
         if($this->isNewRecord){
             $this->create_time = strtotime('now');
             $this->status = self::STATUS_SUCCESS;
-            $this->pay_status = 'paid';
+            $this->pay_status = self::PAY_NO;
+            switch($this->type){
+                case self::TYPE_VISA:
+                    $this->sub_status = self::SUBSTATUS_VISA_NEW;
+                    break;
+            }
         }
         return true;
     }
