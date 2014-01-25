6295faeba6a92ed6266d391eb3b1636cdebaa2ee
Suley <dearsuley@gmail.com>
2014-01-25 12:44:22 +0800

    Asset import

---
+++ trunk/jinmi/protected/views/test/img.php
@@ -0,0 +1,9 @@
+<?php $this->beginWidget('application.extensions.thumbnailer.Thumbnailer', array(
+        'thumbsDir' => 'images/thumbs',
+        'thumbWidth' => 50,// Optional
+    )
+); ?>
+ 
+<img src="images/archlogo.png" />
+ 
+<?php $this->endWidget(); ?>
