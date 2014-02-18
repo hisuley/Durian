a26974211cb287344583a83c45b36a62881a5a0e
Suley <dearsuley@gmail.com>
2014-02-18 11:13:58 +0800

    Create database migrations files

--- trunk/jinmi/protected/migrations/m140218_030547_add_sent_visa_travel_agency_column.php
+++ trunk/jinmi/protected/migrations/m140218_030547_add_sent_visa_travel_agency_column.php
@@ -4,12 +4,12 @@ class m140218_030547_add_sent_visa_travel_agency_column extends CDbMigration
 {
 	public function up()
 	{
+        $this->addColumn('visa_order', 'sent_agency_source', 'int');
 	}
 
 	public function down()
 	{
-		echo "m140218_030547_add_sent_visa_travel_agency_column does not support migration down.\n";
-		return false;
+        $this->dropColumn('visa_order', 'sent_agency_source');
 	}
 
 	/*
