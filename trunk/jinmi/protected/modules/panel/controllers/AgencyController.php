e2556104b171d67fec7ac042185d4297c5a9f0fe
Suley <dearsuley@gmail.com>
2014-06-04 10:45:28 +0800

    Ingore files

--- trunk/jinmi/protected/modules/panel/controllers/AgencyController.php
+++ trunk/jinmi/protected/modules/panel/controllers/AgencyController.php
@@ -5,7 +5,7 @@ class AgencyController extends PanelController
     public $label = "送签旅行社";
     public $subMenu;
     public function getLabel($labelName){
-        $label = array('list'=>'列表', 'delete'=>'删除', 'new'=>'新增', 'update'=>'编辑');
+        $label = array('list'=>'列表', 'delete'=>'删除', 'new'=>'新增', 'update'=>'编辑', 'export'=>'导出');
         return $label[$labelName];
     }
     public function beforeAction(){
@@ -77,11 +77,40 @@ class AgencyController extends PanelController
     }
 
     public function actionExport(){
+        //Open a file pointer
+        $fp = fopen('/tmp/export-way'.strtotime('now').rand(111,999).'.csv', 'w+');
         $models = OrderSource::model()->findAllByAttributes(array('type'=>OrderSource::TYPE_AGENCY));
         $agencyWayData = array();
         foreach($models as $key=>$model){
-           $agencyWayData[$key]['name'] = 
+            $row = array(" ");
+            fputcsv($fp, $row);
+            $row = array("送签社", $model->name, "联系人", $model->contact_name);
+
+            $tempAgencyWay = VisaTypeAgency::model()->findAllByAttributes(array('agency_id'=>$model->id));
+            $agencyWayData[$key]['ways'] = array();
+            if(!empty($tempAgencyWay)){
+                $row = array("编号", "类型", "成本价", "预测出签");
+                foreach($row as $tKey=>$tVal){
+                    $row[$tKey] = iconv('utf-8', 'GBK//IGNORE', $tVal);
+                }
+                fputcsv($fp, $row);
+                foreach($tempAgencyWay as $tKey=>$tVal){
+                    $row = array(($tKey+1), $tVal->type->name, $tVal->price, $tVal->predict_date);
+                    foreach($row as $ttKey=>$ttVal){
+                        $row[$ttKey] = iconv('utf-8', 'GBK//IGNORE', $ttVal);
+                    }
+                    fputcsv($fp, $row);
+                }
+                $row = array("总计", count($tempAgencyWay));
+                foreach($row as $tKey=>$tVal){
+                    $row[$tKey] = iconv('utf-8', 'GBK//IGNORE', $tVal);
+                }
+                fputcsv($fp, $row);
+            }
         }
+        rewind($fp);
+        Yii::app()->request->sendFile('签证渠道_'.date("Y年m月d日").'.csv',stream_get_contents($fp), 'text/csv; charset=GBK//IGNORE');
+        fclose($fp);
     }
 
 }
